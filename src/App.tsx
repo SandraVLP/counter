5a63a9945c7b398d3af4be29940eeca74e43c82b
SandraVLP <aleksanvp@yandex.ru>
2024-04-29 19:28:05 +0300

    max-start values working

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
-import React, { useState } from "react";
-import logo from "./logo.svg";
+import React, { useState, ChangeEvent } from "react";
 import "./App.css";
 import s from "./App.module.css";
 
 function App() {
-  const [count, setCount] = useState<number>(0);
+  const [maxValue, setMaxValue] = useState<number>(0);
+  const [startValue, setStartValue] = useState<number>(0);
+  const [count, setCount] = useState<number>(startValue);
+
+  const handleMaxValueChange = (e: any) => {
+    setMaxValue(parseInt(e.target.value, 10));
+  };
+
+  const handleStartValueChange = (e: any) => {
+    setStartValue(parseInt(e.target.value, 10));
+  };
 
   const handleIncClick = () => {
-    if (count < 5) {
-      setCount(count + 1);
+    if (count < maxValue) {
+      setCount((count) => count + 1);
     }
   };
 
@@ -16,31 +25,58 @@ function App() {
     setCount(0);
   };
 
-  const h1Classname = s.number + " " + (count === 5 ? s.red : " ");
+  const handleSet = () => {
+    if (startValue < maxValue) {
+      setMaxValue(maxValue);
+      setStartValue(startValue);
+      setCount(startValue);
+      console.log(maxValue, startValue);
+    }
+  };
+
+  const h1Classname = s.number + " " + (count === maxValue ? s.red : " ");
 
   return (
     <div className={s.App}>
-            <div className={`${s.container} ${s.outline}`}>
+      <div className={`${s.container} ${s.outline}`}>
         <div className={`${s.container} ${s.inner} ${s.h} ${s.outline}`}>
-        <label htmlFor="maxvalue" className={s.label}>max-value: <input className={s.input} type="number" id="maxvalue" /></label>
-        <label htmlFor="startvalue" className={s.label}>start-value: <input className={s.input} type="number" id="startvalue" /></label>
+          <label htmlFor="maxvalue" className={s.label}>
+            max-value:{" "}
+            <input
+              className={s.input}
+              type="number"
+              id="maxvalue"
+              onChange={handleMaxValueChange}
+              value={maxValue}
+            />
+          </label>
+          <label htmlFor="startvalue" className={s.label}>
+            start-value:{" "}
+            <input
+              className={s.input}
+              type="number"
+              id="startvalue"
+              value={startValue}
+              onChange={handleStartValueChange}
+            />
+          </label>
         </div>
         <div className={`${s.container} ${s.inner}`}>
-
-          <button type="submit" className={s.button} onClick={() => {}}>
+          <button type="submit" className={s.button} onClick={handleSet}>
             Set
           </button>
         </div>
       </div>
       <div className={`${s.container} ${s.outline}`}>
         <div className={`${s.container} ${s.inner} ${s.h}`}>
+          {/* <input type="number" className={h1Classname} value={count}  onChange={handleCountValueChange}/> */}
           <h1 className={h1Classname}>{count}</h1>
         </div>
         <div className={`${s.container} ${s.inner}`}>
           <button
-            disabled={count === 5}
+            disabled={count === maxValue}
             type="button"
-            className={`${s.button} ${count === 5 ? s.disabled : ""}`}
+            className={`${s.button} ${count === maxValue ? s.disabled : ""}`}
             onClick={handleIncClick}
           >
             Inc
@@ -50,9 +86,6 @@ function App() {
           </button>
         </div>
       </div>
-
-
-      
     </div>
   );
 }
