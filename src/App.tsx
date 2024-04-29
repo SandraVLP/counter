78c20ab7e2399c821acad509632f2b1e4049566d
SandraVLP <aleksanvp@yandex.ru>
2024-04-29 16:50:47 +0300

    redcolor 5

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import s from "./App.module.css";
 
 function App() {
 const [count, setCount] = useState<number>(0);
@@ -15,10 +16,14 @@ const handleResetClick = () => {
   setCount(0)
 }
 
+const h1Classname =     s.button +
+" " +
+(count === 5 ? s.red : " ");
+
   return (
     <div className="App">
 <div className='counter-container'>
-  <div className='count'><h1>{count}</h1></div>
+  <div className='count'><h1 className={h1Classname}>{count}</h1></div>
   <div className='calc-container'>
     <button onClick={handleIncClick}>Inc</button>
     <button onClick={handleResetClick}>Reset</button> 
